090834c66f435c5245a91405f08370df40d2b091
TheVeryDarkness <3266343194@qq.com>
2023-12-23 23:09:36 +0800

    Call is_regular_file and file_size in cpp.

--- test-with-cpp/test.cpp
+++ test-with-cpp/test.cpp
@@ -3,6 +3,9 @@
 int main() {
   std::filesystem::recursive_directory_iterator iter("src");
   for (auto f : iter) {
-    std::cout << f.path() << std::endl;
+    std::cout << f.path() << ' ' << f.is_directory();
+    if (f.is_regular_file())
+      std::cout << ' ' << f.file_size();
+    std::cout << std::endl;
   }
 }
